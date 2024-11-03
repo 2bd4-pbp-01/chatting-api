f616edfa8833a36415581d0e4cf97e89cdec7363
Jelang Anugrah <93866226+JelangA@users.noreply.github.com>
2024-11-04 00:54:15 +0700

    Update index.js
    fix komen ngawur

--- chatting-services/index.js
+++ chatting-services/index.js
@@ -8,12 +8,8 @@ app.use(bodyParser.json());
 // Inisialisasi Firebase
 initializeFirebase();
 
-// Rute untuk grup
 app.use('/', require('./routes/index'));
 
-
-
-// Jalankan server pada port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
